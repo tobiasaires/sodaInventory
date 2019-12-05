dea575ff3d1d2237afb7c6086a42ba10844fc17f
Tobias Aires <tobias.aires@secrel.net.br>
2019-12-05 14:12:28 -0300

    fix(config): setting default namespace to api routes

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,12 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::group(['prefix' => 'soda'], function () {
-    Route::post('create', 'Api\SodaController@create');
+    Route::post('create', 'SodaController@create');
 });
